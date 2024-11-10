2dce31bed20911a437efc97ab07f929d1874c537
Pritam Rane <pritamrrane@gmail.com>
2024-11-10 23:54:22 +0530

    removed unwanted

--- src/main/java/com/example/SpringSecurity/controller/UserController.java
+++ src/main/java/com/example/SpringSecurity/controller/UserController.java
@@ -3,6 +3,7 @@ package com.example.SpringSecurity.controller;
 
 import com.example.SpringSecurity.model.User;
 import com.example.SpringSecurity.services.UserService;
+import jakarta.servlet.http.HttpServletRequest;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.*;
 
@@ -17,6 +18,11 @@ public class UserController {
     public User register(@RequestBody User user){
         return userService.register(user);
 
+    }
+
+    @GetMapping("/hello")
+    public String getHello(HttpServletRequest request){
+        return "Hello "+ request.getSession().getId();
     }
 
     @PostMapping("/login")
--- src/main/java/com/example/SpringSecurity/services/UserService.java
+++ src/main/java/com/example/SpringSecurity/services/UserService.java
@@ -7,6 +7,7 @@ import org.springframework.security.authentication.AuthenticationManager;
 import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
 import org.springframework.security.core.Authentication;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.crypto.password.Pbkdf2PasswordEncoder;
 import org.springframework.stereotype.Service;
 
 @Service
@@ -22,7 +23,7 @@ public class UserService {
     @Autowired
     private AuthenticationManager manager;
 
-    private BCryptPasswordEncoder encoder = new BCryptPasswordEncoder(13);
+    private BCryptPasswordEncoder encoder = new BCryptPasswordEncoder(13); //encodes only first 72 chars
 
 
     public User         register(User user){
